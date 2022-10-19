669f2193840c8460ac2ebb1e60a002bd367e738f
Benjamin Prentiss <bschenk18@yahoo.com>
2022-10-19 12:57:12 -0600

    finished demo

---
+++ varsAndConditionals.js
@@ -0,0 +1,48 @@
+/*
+    In this file, we'll be using variables, conditionals,
+    and loops to play out a fight between Obiwan Kenobi and
+    Anakin Skywalker.
+*/ 
+
+let kenobiAttack = 25
+let skywalkerAttack = 35
+
+if(kenobiAttack > skywalkerAttack){
+    console.log(`Obiwan has a better attack`)
+} else if(kenobiAttack < skywalkerAttack){
+    console.log(`Anakin has the better attack`)
+}
+else {
+    console.log(`Obbiwan and Anakin have the same attack`)
+}
+
+let kenobiHealth = 100
+let kenobiDefense = 0
+
+if(skywalkerAttack >= kenobiHealth){
+    console.log(`Obiwan was slain`)
+} else {
+    kenobiHealth = kenobiHealth - skywalkerAttack
+    console.log(`Obiwan's health is now ` + kenobiHealth)
+}
+
+kenobiDefense += 30
+
+if ((kenobiHealth + 50) >= 100){
+    kenobiHealth = 100
+} else {
+    kenobiHealth += 50
+}
+
+for(let i = 0;  i < 5; i++){
+    kenobiHealth -= (skywalkerAttack - kenobiDefense)
+    console.log(`Obiwan's health is now ${kenobiHealth}`)
+}
+
+while(kenobiHealth > 0){
+    kenobiHealth -= (skywalkerAttack - kenobiDefense)
+    console.log(kenobiHealth)
+    if(kenobiHealth <= 0){
+        console.log(`Obiwan has been slain`)
+    }
+}
